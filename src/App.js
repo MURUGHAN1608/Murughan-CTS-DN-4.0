e6755304f4ea09ce406f3c9dffcd2049ea84debf
MURUGHAN1608 <murughanbalaji21@gmail.com>
2025-07-27 23:08:44 +0530

    Initial commit - uploading React project to existing GitHub repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CalculateScore from './Components/CalculateScore';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CalculateScore name="John Doe" school="ABC High School" total={450} goal={5} />
     </div>
   );
 }
